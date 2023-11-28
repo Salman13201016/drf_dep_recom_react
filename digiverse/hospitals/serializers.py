8574d1488b0ab2d46e0b8e9c5f2052621a1a3889
nirban007 <nirbanmitu@gmail.com>
2023-11-29 03:57:08 +0600

    hospital_map_app created successfully

--- digiverse/hospitals/serializers.py
+++ digiverse/hospitals/serializers.py
@@ -1,4 +1,3 @@
-# hospitals/serializers.py
 from rest_framework import serializers
 from .models import Hospital
 from divisions.models import Division
@@ -6,33 +5,12 @@ from districts.models import District
 from hospital_categories.models import HospitalCategory
 from stations.models import Station
 
-class DivisionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Division
-        fields = ('id', 'name')
-
-class DistrictSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = District
-        fields = ('id', 'name')
-
-class HospitalCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HospitalCategory
-        fields = ('id', 'name')
-
-class StationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Station
-        fields = ('id', 'name')
-
 class HospitalSerializer(serializers.ModelSerializer):
-    division = DivisionSerializer()
-    district = DistrictSerializer()
-    hos_type = HospitalCategorySerializer()
-    station = StationSerializer()
-
     class Meta:
         model = Hospital
         fields = '__all__'
 
+    division = serializers.PrimaryKeyRelatedField(queryset=Division.objects.all())
+    district = serializers.PrimaryKeyRelatedField(queryset=District.objects.all())
+    hos_type = serializers.PrimaryKeyRelatedField(queryset=HospitalCategory.objects.all())
+    station = serializers.PrimaryKeyRelatedField(queryset=Station.objects.all())
