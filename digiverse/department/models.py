1d0f0687cfef37e5344407f7fc6898f171d692e1
nirban007 <nirbanmitu@gmail.com>
2023-12-11 11:40:29 +0600

    Test commit

--- digiverse/department/models.py
+++ digiverse/department/models.py
@@ -1,10 +1,13 @@
 # department/models.py
 from django.db import models
-
+from hospitals.models import Hospital
 class Department(models.Model):
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     details = models.TextField(max_length=255)
 
     def __str__(self):
         return self.name
+
+#Nirban007
 
