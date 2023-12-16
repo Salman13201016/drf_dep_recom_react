75a21fa7ab058c8db1af54aac88450427185c30d
nirban007 <57198481+nirban007@users.noreply.github.com>
2023-12-16 12:02:16 +0600

    registration backend updated

--- digiverse/registration/urls.py
+++ digiverse/registration/urls.py
@@ -1,8 +1,8 @@
 # registration/urls.py
 from django.urls import path
-from .views import register, email_verification
+from .views import RegistrationViewSet
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('email-verification/<int:user_id>/', email_verification, name='email_verification'),
+     path('register/', RegistrationViewSet.as_view({'post': 'create', 'get': 'list'}), name='registration-list'),
+    # path('email-verification/<int:user_id>/', email_verification, name='email_verification'),
 ]
