15d803b826e77a93dec3625f4664ea4edae4a580
nirban007 <nirbanmitu@gmail.com>
2024-01-04 23:39:34 +0600

    commit for pull

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dell\Desktop\drf_dep_recom_react\myenv
+set VIRTUAL_ENV=C:\Users\dell\Desktop\DRF\drf_dep_recom_react\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
