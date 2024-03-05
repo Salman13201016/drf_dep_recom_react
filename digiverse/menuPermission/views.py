368cc7ceed942bab3e16628f397388f2b65b1811
nirban007 <nirbanmitu@gmail.com>
2024-03-05 21:31:16 +0600

    user_role, role_permission, auth_user updated

--- digiverse/menuPermission/views.py
+++ digiverse/menuPermission/views.py
@@ -16,7 +16,26 @@ class MenuRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MenuSerializer
     # permission_classes = [MenuPermission]
 
+# Assuming your view is using a ViewSet
+
 class MenuPermissionViewSet(viewsets.ModelViewSet):
-    queryset  = MenuPermission.objects.all()
+    queryset = MenuPermission.objects.all()
     serializer_class = MenuPermissionSerializer
+
+    # Override the create method to handle the creation of permissions with submenus
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    # Override the update method to handle updating permissions with submenus
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
 
--- digiverse/user_role/admin.py
+++ digiverse/user_role/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import user_role_management
+admin.site.register(user_role_management)
